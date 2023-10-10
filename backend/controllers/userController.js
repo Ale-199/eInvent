cf3d21b4410c4952defee318593bd7977036d20a
Alejandro ZZ <ale0720199@gmail.com>
2023-10-10 16:24:24 -0400

    backend change password controller function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,10 @@
 const User = require("../models/userModel");
+const ResetToken = require("../models/resetTokenModel");
+const sendEmail = require("../utils/sendEmail");
+
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const ResetToken = require("../models/resetTokenModel");
 const crypto = require("crypto");
 
 const generateToken = (id) => {
@@ -116,7 +118,109 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+//Logout user
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("token", "", {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(0), // 1 day
+    sameSite: "none",
+    secure: true,
+  });
+  return res.status(200).json({
+    message: "Successfully logged out.",
+  });
+});
+
+//Get a single User
+const getUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+//Get login status
+const loginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json(false);
+  }
+
+  //verify token
+  const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (verified) {
+    return res.json(true);
+  }
+});
+
+//Update user
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user_id);
+  if (user) {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      {
+        ...req.body,
+      }
+    ).select("-password");
+    res.status(200).json(updatedUser);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+const changePassword = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  const { oldPassword, password } = req.body;
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found, please sign up.");
+  }
+
+  if (!oldPassword || !password) {
+    res.status(400);
+    throw new Error("Please add old password and new password.");
+  }
+
+  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+  if (user && passwordIsCorrect) {
+    user.password = password;
+    await user.save();
+    res.status(200).send("Password change successfully.");
+  } else {
+    res.status(400);
+    throw new Error("Old password is incorrect");
+  }
+});
+
+const forgotPassword = asyncHandler(async (req, res) => {
+    
+});
+
 module.exports = {
   registerUser,
   loginUser,
+  logoutUser,
+  getUser,
+  loginStatus,
+  updateUser,
+  changePassword,
+  forgotPassword,
 };
